8d869703a8bf0ea0aee20705e8d8960c01fba97a
Lukas Sawon <lukash_savon@mail.ru>
2022-05-23 22:43:15 +0200

    Change vercel build script

--- vercel.sh
+++ vercel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "BRANCH_TO_BUILD: $BRANCH_TO_BUILD"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "$BRANCH_TO_BUILD"  ]] ; then
   # Proceed with the build
 	echo "✅ - Build can proceed"
   exit 1;
