1b882ef6ce954d56dbb3c3dc6ae5b16b23185705
Lukas Sawon <lukash_savon@mail.ru>
2022-05-23 22:46:50 +0200

    Add vercel build script

---
+++ vercel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "BRANCH_TO_BUILD: $BRANCH_TO_BUILD"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "$BRANCH_TO_BUILD"  ]] ; then
+  # Proceed with the build
+	echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
